0b726f0abeefca4a186b060b6afd739ea2ff5cc2
Yashasvi Goswami <yashasvig43@gmail.com>
2026-03-11 12:32:15 +0530

    Embed Search and Upcoming sections. Learned how apple UI works and how to find Symbols, as well as dynamic changes to Icon function when tapped/clicked
    Understanding the app structure and overall code, motivated to proceed Swift learning.

---
+++ BlossomMovie/UpcomingView.swift
@@ -0,0 +1,18 @@
+//
+//  UpcomingView.swift
+//  BlossomMovie
+//
+//  Created by Yashasvi Goswami on 11/03/26.
+//
+
+import SwiftUI
+
+struct UpcomingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpcomingView()
+}
---
+++ BlossomMovie/VerticalListView.swift
@@ -0,0 +1,18 @@
+//
+//  VerticalListView.swift
+//  BlossomMovie
+//
+//  Created by Yashasvi Goswami on 11/03/26.
+//
+
+import SwiftUI
+
+struct VerticalListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VerticalListView()
+}
